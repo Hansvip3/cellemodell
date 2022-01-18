a05bfe5a9b38c2aa4f5779cafb2128fb6eda607d
Hansvip3 <hans.fjellstad@outlook.com>
2022-01-18 12:21:45 +0100

    secondary file update

--- celle/celle.go
+++ celle/celle.go
@@ -2,7 +2,27 @@ package cell
 
 func InitCell() bool { return false }
 
+
+
 /*
    INN-DATA --> FUNKSJON --> UT-DATA
       ()        InitCell      etter InitCell()
 */
+
+
+var cell bool
+
+func initCell() bool {
+    cell = false;
+    return cell
+}
+
+
+func SetCellValue(value bool) bool {
+    cell = value;
+    return cell;
+}
+
+func GetCellValue() bool {
+    return cell
+}
--- celle/celle_test.go
+++ celle/celle_test.go
@@ -9,3 +9,22 @@ func TestInitCell(t *testing.T) {
         t.Errorf("Feil, fikk %t, ønsket %t.", state, wanted)
     }
 }
+
+
+func TestSetCellValue(t *testing.T) {
+    wanted := true; // Bruker semantikken i Golangs konseptuelle modell
+    state := SetCellValue(true);
+    if state != wanted {
+        t.Errorf("Feil, fikk %t, ønsket %t.", state, wanted)
+    }
+}
+
+
+func TestGetCellValue(t *testing.T) {
+    wanted := true; // Bruker semantikken i Golangs konseptuelle modell
+    SetCellValue(true)
+    state := GetCellValue();
+    if state != wanted {
+        t.Errorf("Feil, fikk %t, ønsket %t.", state, wanted)
+    }
+}
